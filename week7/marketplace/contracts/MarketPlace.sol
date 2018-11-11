f9f40a7f99b743dc32c213b58b67c9cd31770078
Victor Danilenko <victor.fog@gmail.com>
2018-11-11 13:12:45 +0300

    ading voiting

--- week7/marketplace/contracts/MarketPlace.sol
+++ week7/marketplace/contracts/MarketPlace.sol
@@ -16,7 +16,9 @@ contract MarketPlace {
 
     }
 
-    struct oneOrder { // планируется для отслеживания подтверждения, что все получили чего хотели.
+    enum statusDisput {empty,exist,close}
+
+    struct oneOrder {// планируется для отслеживания подтверждения, что все получили чего хотели.
         //uint OrderID;
         uint FileID;
         address BayerAddress;
@@ -24,8 +26,8 @@ contract MarketPlace {
         bool BayerApprove; // согласие завершение сделки со стороны покупателя
         uint FixPrise; // стоимость для фиксирования цены
         bool IsPayed;
-        uint ExistDisput; //todo добавить поле открытого спора
-
+        statusDisput ExistDisput;
+        uint DisputID;// id спора
 
         // берем из dbFiles[oneOrder.FileID]
         //        string Name;
@@ -33,30 +35,65 @@ contract MarketPlace {
         //        bytes32 SwarmHash;
         //        uint Price;
         //        string Description;
+
+    }
+
+    struct arbitrator {
+        address arbitratorAddress;
+        // uint ID;
+        uint DisputCount;
+        uint Rating;
+        uint Deposit; ///всю инфу хранитьтут мапинг по адресу на все данные арбитра
+        bool Exist;
         //
     }
 
+
+    struct disput {//todo: структура споров в массиве и id спора записывать продавцу
+        uint DateCreateDisput;
+        address WhoCreateDisput;
+        string Complaint; //суть притензии
+        string AnswerComplaint; // ответочка
+        bool ConsentOwner;
+        bool ConsentBayer;
+        bool CallArbitr;
+        string arbirtatorComment;
+        uint arbitr1;
+        bytes32 arbitr1vote; // кто из арбитров 1 2 3
+        uint arbitr2;
+        bytes32 arbitr2vote;
+        uint arbirt3;
+        bytes32 arbitr3vote;
+    }
+    // fixme насморк замучил немогу, только и бегаю до ванны и обратно 5 утра поеду в аптеку ((((
+    //mapping(address=>uint[]) arbitrationDisputs; // не уверен что эир мне надо
+
     event NewFile(string _name, bytes32 _Hash, bytes32 _SwarmHash, uint _Price,
-            string _Description, uint _FileID, uint _SellerID, address _owner);
+        string _Description, uint _FileID, uint _SellerID, address _owner);
 
     //  mapping(address => sFile[]) dbFile;
     sFile[] dbFiles;
     mapping(address => uint[]) sellerFileIDs;
-   // mapping(address => uint) deposit;
+    //mapping(uint => arbitrator[]) arbitratorDB; //ID арбитра на структуру с всеми данными по ниму
+    // address[] arbitratorID; //массив адресов арбитров номер - адрес
+    // mapping(address => uint) deposit;
 
     oneOrder[] allOrders;
+    arbitrator[] allArbitrator; //арбитры
     mapping(uint => address) ownerOrdersID;
     mapping(uint => address) bayersOrdersID;
+    mapping(address => uint) arbitratorID;
+    mapping(uint => disput) allDisput;
 
-    //todo структура споров в массиве и id спора записывать продавцу
-    //массив для споров
 
-
+    //disput[] allDisput; //сюда будем заносить споры fixme: переделать на mapping uint - > disput ( где uint orderID )
     //todo (возможно создать структуру покупателя с занесением информации о спорах)
+
+    //mapping //идея заключается в сохранении номерСпора=>адбитры
 
 
     address[] allVendorsAtTheCurrentMoment; // для проверки: есть ли такой продавец
-
+    // todo при создании файла нужно внести депозит
     function addFile(string _name, bytes32 _Hash, bytes32 _SwarmHash, uint _Price, string _Description) public {
         uint _fileCount = sellerFileIDs[msg.sender].length;
         uint _SellerID;
@@ -75,16 +112,15 @@ contract MarketPlace {
         emit NewFile(_name, _Hash, _SwarmHash, _Price, _Description, _FileID, _SellerID, msg.sender);
     }
 
-//    function list() public view returns(sFile[]) {
-//        sFile[] memory _allfiles;
-//        for (uint i = 0; i < dbFiles.length; i++) {
-//            _allfiles.push(dbFiles[i]);
-//        }
-//        return _allfiles;
-//
-//    }
+    //    function list() public view returns(sFile[]) {
+    //        sFile[] memory _allfiles;
+    //        for (uint i = 0; i < dbFiles.length; i++) {
+    //            _allfiles.push(dbFiles[i]);
+    //        }
+    //        return _allfiles;
+    //
+    //    }
 
-    mapping(address => uint) deposits;
 
     event EventCreateOrder(uint _FileID, uint _orderID, uint _price);
 
@@ -92,8 +128,8 @@ contract MarketPlace {
         sFile memory BayFile = dbFiles[_FileID];
         require(dbFiles.length >= _FileID, "Файл с указанным ID не существует");
         require(BayFile.Price <= msg.value, "given money should be greater than the price");
-//      deposits[msg.sender] += msg.value;
-        uint _orderID = allOrders.push(oneOrder(_FileID, msg.sender, false, false, BayFile.Price, false, 0))-1;
+        uint _orderID = allOrders.push(oneOrder(_FileID, msg.sender, false, false, BayFile.Price, false, statusDisput.empty , 0)) - 1;
+        //0- статус спора; 0-ID спора
         ownerOrdersID[_orderID];
         bayersOrdersID[_orderID];
         emit EventCreateOrder(_FileID, _orderID, BayFile.Price);
@@ -119,7 +155,7 @@ contract MarketPlace {
             emit eventApproveOrder(_order.OwnerApprove, _order.BayerApprove, "no transaction");
         }
         // если она true вызвать closeOrder
-        if(_order.OwnerApprove == true && _order.BayerApprove == true){
+        if (_order.OwnerApprove == true && _order.BayerApprove == true) {
             closeOrder(_orderID);
             emit eventApproveOrder(_order.OwnerApprove, _order.BayerApprove, "Transaction");
         }
@@ -139,9 +175,9 @@ contract MarketPlace {
         }
     }
 
-    function searchOrder(uint _fileID) view public returns(uint) {
+    function searchOrder(uint _fileID) view public returns (uint) {
         oneOrder memory _order;
-        for (uint i=0; i < allOrders.length; i++) {
+        for (uint i = 0; i < allOrders.length; i++) {
             _order = allOrders[i];
 
             if (msg.sender != _order.BayerAddress) {
@@ -155,10 +191,11 @@ contract MarketPlace {
         }
     }
 
-    function canselOrder(uint _orderID) public { //todo создать функцию отмена заказа
-        oneOrder storage _order =  allOrders[_orderID];
-        require(_order.OwnerApprove != true && _order.BayerApprove != true);
+    function canselOrder(uint _orderID) public {//todo создать функцию отмена заказа
+        oneOrder storage _order = allOrders[_orderID];
+        require(_order.OwnerApprove == false && _order.BayerApprove == false);
         require(_order.IsPayed == false, "the order has been already payed");
+        require(_order.ExistDisput != statusDisput.exist);
         address _bayerAddress = _order.BayerAddress;
         require(_bayerAddress == msg.sender);
         uint amount = _order.FixPrise;
@@ -168,9 +205,121 @@ contract MarketPlace {
 
     }
 
-//    function disput(uint _orderID, string _complaint) public {
-//        oneOrder storage _order = allOrders[_orderID];
-//        sFile storage _fileInfo = dbFiles[_order.FileID];
-//    }
+    uint constant minArbitorDeposit = 10;
 
+    function addDepositArbitrator() public payable { // нужен тест
+        uint _ID1 = arbitratorID[msg.sender];
+        require(allArbitrator[_ID1].Exist == true);
+        allArbitrator[_ID1].Deposit += msg.value;
+    }
+
+    function becomeANarbitrator() public payable { //нужен тетт
+        require(minArbitorDeposit <= msg.value);
+        arbitratorID[msg.sender] = newArbitrator(msg.sender, msg.value);
+    }
+
+    function newArbitrator(address arbitr, uint _deposit) private returns (uint) { //тужен тест
+        return allArbitrator.push(arbitrator({
+            arbitratorAddress : arbitr,
+            Deposit : _deposit,
+            DisputCount : 0,
+            Rating : 0,
+            Exist : true
+            })) - 1;
+    }
+
+    event createDisputEvent(uint _orderID, string _complaint, address _owner, address _bayer); //нужен тест
+
+    function createDisput(uint _orderID, string _complaint) public {
+        oneOrder storage _order = allOrders[_orderID];
+        sFile storage _fileInfo = dbFiles[_order.FileID];
+        require(_order.BayerAddress == msg.sender || _fileInfo.Owner == msg.sender);
+        require(_order.ExistDisput == statusDisput.empty);
+        _order.ExistDisput = statusDisput.exist;
+        allDisput[_orderID] = disput({
+            DateCreateDisput: now,
+            WhoCreateDisput: msg.sender,
+            Complaint: _complaint,
+            AnswerComplaint: "",
+            ConsentOwner: false,
+            ConsentBayer: false,
+            CallArbitr: false,
+            arbirtatorComment:"",
+            arbitr1: 0 //fixme: надо настроить на пустой параметр или адрес контракта или владельца ресурса.
+            arbitr1vote: empty,
+            arbitr2: 0,
+            arbitr2vote: empty,
+            arbirt3: 0,
+            arbitr3vote: empty
+            });
+
+        //todo обращаемся к базе арбитров и назначаем 3х красавцев
+    }
+
+    function callArbitrator (uint _orderID, string _comments) public { //нужен тест
+        disput storage _disput = allDisput[_orderID];
+        require(_disput.CallArbitr == false);
+        require(_disput.DateCreateDisput + 10 days < now);
+        _disput.arbitr1 = 0;
+        _disput.arbirtatorComment = _comments;
+        _disput.CallArbitr = true;
+
+    }
+
+    function setArbitr (uint _orderID) public returns(uint){ ///Нужен выбор арбитров
+        uint _who; //fixme переделать на вызов функции рандомного выбора арбитров
+        disput storage _disput = allDisput[_orderID];
+        _disput.arbitr1 = _who; //какой-то бред нуден или номер арбитра из массива или его адрес и мап с индексами
+        _disput.arbitr2 = _who;
+        _disput.arbitr3 = _who;
+
+    }
+
+
+
+
+
+// fixme голосование ____ отметка все что ниже надо редактировать
+// fixme
+
+   // structVoting voting; // не нужна вместо нее все в disput
+//    mapping(bool => address[]) votes;// старая проверка на произведено голосование или нет //fixme на
+//    mapping(address => bool) usersDb;// todo: в мапинге надо хранить хеш голосов
+//    mapping(bytes32 => address) hashBd;
+//
+//    event XXX(uint256);
+//    event voteX(bool _vote, bytes32 _word);
+
+
+    function vote(bytes32 _voteHash) public {
+        require(voting.startDate <= now);
+        require(voting.endDate >= now);
+        require(usersDb[msg.sender] != true);
+
+        usersDb[msg.sender] = true; // помечаем кто проголосовал
+        hashBd[_voteHash] = msg.sender;
+    }
+
+    function hashVote(bool _vote, bytes32 _word) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_vote, _word));
+    }
+
+    function openvote(bool _vote, bytes32 _word) public {
+        require(voting.endDate <= now);
+        require(usersDb[msg.sender]);
+
+        if (hashBd[hashVote(_vote, _word)] == msg.sender){
+            votes[_vote].push(msg.sender);
+        }
+    }
+
+    function stats() public view returns (uint, uint) {
+        address[] memory _arrayVotesNo = votes[false];
+        address[] memory _arrayVotesYes = votes[true];
+        uint no = _arrayVotesNo.length;
+        uint yes = _arrayVotesYes.length;
+
+        return (no, yes);
+
+    }
 }
